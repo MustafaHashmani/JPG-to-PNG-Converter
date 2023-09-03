cc3c7211786857ec32f0106cdd60b5ded99b65f2
Mustafa Hashmani <63756923+MustafaHashmani@users.noreply.github.com>
2023-09-03 07:05:19 +0500

    add boilerplate code

---
+++ converter.py
@@ -0,0 +1,11 @@
+import sys
+import os
+from PIL import Image
+
+# grab first and second argument
+
+# check if new/ exists and if it does not then create it
+
+# loop through folder and convert images to PNG
+
+# save to the new folder
